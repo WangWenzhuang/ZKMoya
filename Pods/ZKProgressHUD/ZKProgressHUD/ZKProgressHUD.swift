057e6378263bb4c59a51ed0b33ab0c51ab03d247
王文壮 <1020304029@qq.com>
2019-03-19 18:56:04 +0800

    更新第三方

--- Pods/ZKProgressHUD/ZKProgressHUD/ZKProgressHUD.swift
+++ Pods/ZKProgressHUD/ZKProgressHUD/ZKProgressHUD.swift
@@ -59,7 +59,7 @@ public class ZKProgressHUD: UIView {
     }
     
     
-    fileprivate var blurEffectStyle: UIBlurEffectStyle {
+    fileprivate var blurEffectStyle: UIBlurEffect.Style {
         get {
             switch Config.effectStyle {
             case .extraLight:
@@ -111,7 +111,7 @@ public class ZKProgressHUD: UIView {
     }
     
     fileprivate lazy var systemHUDView = UIActivityIndicatorView().then {
-        $0.activityIndicatorViewStyle = .whiteLarge
+        $0.style = .whiteLarge
         $0.color = Config.foregroundColor
         $0.sizeToFit()
         $0.startAnimating()
@@ -122,11 +122,17 @@ public class ZKProgressHUD: UIView {
         y: 0,
         width: 65,
         height: 65
-    )).then {
+    )).then({ view in
         let lineWidth: CGFloat = 3
         let lineMargin: CGFloat = lineWidth / 2
-        let arcCenter = CGPoint(x: $0.width / 2 - lineMargin, y: $0.height / 2 - lineMargin)
-        let smoothedPath = UIBezierPath(arcCenter: arcCenter, radius: $0.width / 2 - lineWidth, startAngle: 0, endAngle: CGFloat(Double.pi * 2), clockwise: true)
+        let arcCenter = CGPoint(x: view.width / 2 - lineMargin, y: view.height / 2 - lineMargin)
+        let smoothedPath = UIBezierPath(
+            arcCenter: arcCenter,
+            radius: view.width / 2 - lineWidth,
+            startAngle: 0,
+            endAngle: CGFloat(Double.pi * 2),
+            clockwise: true
+        )
         
         let layer = CAShapeLayer().then {
             $0.contentsScale = UIScreen.main.scale
@@ -134,8 +140,8 @@ public class ZKProgressHUD: UIView {
             $0.fillColor = UIColor.clear.cgColor
             $0.strokeColor = Config.foregroundColor.cgColor
             $0.lineWidth = 3
-            $0.lineCap = kCALineCapRound
-            $0.lineJoin = kCALineJoinBevel
+            $0.lineCap = CAShapeLayerLineCap.round
+            $0.lineJoin = CAShapeLayerLineJoin.bevel
             $0.path = smoothedPath.cgPath
             $0.mask = CALayer()
             $0.mask?.contents = Config.bundleImage(.mask)?.cgImage
@@ -153,8 +159,8 @@ public class ZKProgressHUD: UIView {
         
         layer.add(animation, forKey: "rotate")
         
-        $0.layer.addSublayer(layer)
-    }
+        view.layer.addSublayer(layer)
+    })
     
     fileprivate lazy var statusLabel = UILabel().then {
         $0.textAlignment = .center
@@ -509,7 +515,7 @@ extension ZKProgressHUD {
             let window = UIApplication.shared.windows.reversed().first(where: {
                 $0.screen == UIScreen.main &&
                 !$0.isHidden && $0.alpha > 0 &&
-                $0.windowLevel == UIWindowLevelNormal
+                $0.windowLevel == UIWindow.Level.normal
             })
             return window
         }
